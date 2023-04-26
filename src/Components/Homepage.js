36a2c499b71b9b7dc51226b931fee45add348b87
charlie <charlessmith2316@gmail.com>
2023-04-26 10:04:23 -0400

    remove unneccsary

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Homepage() {
-  const [clicked, setClicked] = useState(false);
-  const history = useHistory();
+
+  const history = useHistory()
 
   function homePageClick() {
-    setClicked(true);
-    history.push("/CitiesList");
+    
+    history.push("/CitiesList")
   }
 
   return (
     <div>
-      <h1>Welcome to my website!</h1>
-      {!clicked && (
-        <button onClick={() => homePageClick()}>Click here to enter</button>
-      )}
+      <h1>FunFinder</h1>
+      {
+        <button onClick={() => homePageClick()}>Let's Get Started</button>
+      }
     </div>
   );
 }
